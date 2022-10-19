1844d01f97312ee0ed6470d0ce4b34758371fa5f
KobeBergmans <r0747846@kuleuven.be>
2022-10-19 11:38:50 +0200

    Added sorting of column data per row for CRS

--- Utill/TripletToCRS.hpp
+++ Utill/TripletToCRS.hpp
@@ -13,60 +13,60 @@
 
 namespace pwm {
     template<typename T, typename int_type>
-    void swapArrayElems(int_type* row_coord, int_type* col_coord, T* data, int_type index_1, int_type index_2) {
-        int_type row_temp = row_coord[index_1];
-        int_type col_temp = col_coord[index_1];
-        T data_temp = data[index_1];
-        
-        row_coord[index_1] = row_coord[index_2];
-        col_coord[index_1] = col_coord[index_2];
-        data[index_1] = data[index_2];
+    void swapArrayElems(int_type** coords, T* data, int am_coords, int_type index_1, int_type index_2) {
+        for (int i = 0; i < am_coords; ++i) {
+            int_type temp = coords[i][index_1];
+            coords[i][index_1] = coords[i][index_2];
+            coords[i][index_2] = temp;
+        }
 
-        row_coord[index_2] = row_temp;
-        col_coord[index_2] = col_temp;
+        T data_temp = data[index_1];
+        data[index_1] = data[index_2];
         data[index_2] = data_temp;
     }
 
     template<typename T, typename int_type>
-    int_type partitionArrays(int_type* row_coord, int_type* col_coord, T* data, int_type low, int_type high) {
+    int_type partitionArrays(int_type** coords, T* data, int am_coords, int_type low, int_type high) {
         // Select pivot (rightmost element)
-        int_type pivot = row_coord[high];
+        int_type pivot = coords[0][high];
 
         // Points to biggest element
         int_type i = (low - 1);
         for (int j = low; j < high; j++) {
-            if (row_coord[j] <= pivot) {
+            if (coords[0][j] <= pivot) {
                 // If element is smaller than pivot swap it with i+1
                 i++;
-                swapArrayElems(row_coord, col_coord, data, i, j);
+                swapArrayElems(coords, data, am_coords, i, j);
             }
         }
 
         // Swap pivot with the greatest element at i+1
-        swapArrayElems(row_coord, col_coord, data, i+1, high);
+        swapArrayElems(coords, data, am_coords, i+1, high);
 
         // return the partitioning point
         return i+1;
     }
 
     /**
-     * @brief Quicksort implementation to sort the triplets by rows
+     * @brief Quicksort implementation to sort the triplets by coordinates
+     * 
+     * Sorts all the elements using quicksort by the first array present in coords
      * 
      * https://www.programiz.com/dsa/quick-sort
      * 
-     * @param row_coord Array of row coordinates of Triplet format
-     * @param col_coord Array of column coordinates of Triplet format
+     * @param coords Coordinate array of arrays where the first array represents the coordinate to sort on 
      * @param data Data array for Triplet format
+     * @param am_coords Amount of arrays in coords
      * @param low Start index of quicksort
      * @param high End index of quicksort
      */
     template<typename T, typename int_type>
-    void sortTripletsForCRS(int_type* row_coord, int_type* col_coord, T* data, int_type low, int_type high) {
+    void sortCoordsForCRS(int_type** coords, T* data, int am_coords, int_type low, int_type high) {
         if (low < high) {
-            int_type middle = partitionArrays(row_coord, col_coord, data, low, high);
+            int_type middle = partitionArrays(coords, data, am_coords, low, high);
 
-            sortTripletsForCRS(row_coord, col_coord, data, low, middle - 1);
-            sortTripletsForCRS(row_coord, col_coord, data, middle + 1, high);
+            sortCoordsForCRS(coords, data, am_coords, low, middle - 1);
+            sortCoordsForCRS(coords, data, am_coords, middle + 1, high);
         }
     }
 
@@ -86,7 +86,10 @@ namespace pwm {
     template<typename T, typename int_type>
     void TripletToCRS(int_type* row_coord, int_type* col_coord, T* data, int_type* row_start, int_type* col_ind, T* CRS_data, int_type nnz) {
         // Sort triplets on row value
-        sortTripletsForCRS(row_coord, col_coord, data, 0, nnz-1);
+        int_type** coords = new int_type*[2];
+        coords[0] = row_coord;
+        coords[1] = col_coord;
+        sortCoordsForCRS(coords, data, 2, 0, nnz-1);
 
         // Fill CRS datastructures
         row_start[0] = 0;
@@ -103,8 +106,11 @@ namespace pwm {
 
         row_start[row_index + 1] = nnz;
 
-        // TODO: Sort columns of CRS data
-
+        // Sort columns of CRS data
+        coords[0] = col_ind;
+        for (int row = 0; row <= row_coord[nnz-1]; ++row) {
+            sortCoordsForCRS(coords, CRS_data, 1, row_start[row], row_start[row+1]-1);
+        }
     }
 } // namespace pwm
 
