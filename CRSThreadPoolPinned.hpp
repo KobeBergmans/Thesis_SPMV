dd7b00ba53dbd0542c83653f7d7258684954d909
KobeBergmans <r0747846@kuleuven.be>
2022-10-20 14:02:25 +0200

    Added MM import function to CRSThreadPoolPinned

--- CRSThreadPoolPinned.hpp
+++ CRSThreadPoolPinned.hpp
@@ -47,6 +47,12 @@ namespace pwm {
             // Amount of partitions
             int partitions;
 
+            // Amount of rows per partition
+            int_type* partition_rows;
+
+            // First row of each partition
+            int_type* first_rows;
+
             // Thread pool
             boost::asio::thread_pool pool;
 
@@ -55,6 +61,58 @@ namespace pwm {
 
             // Normalize Function list
             std::vector<std::function<void(T*, T)>> norm_function_list;
+
+        private:
+            void generateFunctions() {
+                int cpu_count = std::thread::hardware_concurrency();
+                int max_threads = std::min(threads, cpu_count);
+                for (int i = 0; i < partitions; ++i) {
+                    // Create mv lambda function for this thread
+                    std::function<void(const T*, T*)> mv_func = [=](const T* x, T* y) -> void {
+                        // Put the current thread on the right cpu
+                        cpu_set_t *mask;
+                        mask = CPU_ALLOC(1);
+                        auto mask_size = CPU_ALLOC_SIZE(1);
+                        CPU_ZERO_S(mask_size, mask);
+                        CPU_SET_S(i % max_threads, mask_size, mask);
+                        if (sched_setaffinity(0, mask_size, mask)) {
+                            std::cout << "Error in setAffinity" << std::endl;
+                        }
+
+                        int_type j;
+                        for (int_type l = 0; l < partition_rows[i]; ++l) {
+                            T sum = 0;
+                            for (int_type k = row_start[i][l]; k < row_start[i][l+1]; ++k) {
+                                j = col_ind[i][k];
+                                sum += data_arr[i][k]*x[j];
+                            }
+                            y[l+first_rows[i]] = sum;
+                        }
+                    };
+
+                    mv_function_list.push_back(mv_func);
+
+
+                    // Create normalize function for this thread
+                    std::function<void(T*, T)> norm_func = [=](T* x, T norm) -> void {
+                        // Put the current thread on the right cpu
+                        cpu_set_t *mask;
+                        mask = CPU_ALLOC(1);
+                        auto mask_size = CPU_ALLOC_SIZE(1);
+                        CPU_ZERO_S(mask_size, mask);
+                        CPU_SET_S(i % max_threads, mask_size, mask);
+                        if (sched_setaffinity(0, mask_size, mask)) {
+                            std::cout << "Error in setAffinity" << std::endl;
+                        }
+
+                        for (int_type l = 0; l < partition_rows[i]; ++l) {
+                            x[l+first_rows[i]] /= norm;
+                        }
+                    };
+
+                    norm_function_list.push_back(norm_func);
+                }
+            }
 
         public:
             // Base constructor
@@ -87,74 +145,57 @@ namespace pwm {
                 col_ind = new int_type*[partitions];
                 data_arr = new T*[partitions];
 
+                partition_rows = new int_type[partitions];
+                first_rows = new int_type[partitions];
+
                 // Generate data for each thread
                 int_type am_rows = std::round(m*n/partitions);
-                int_type first_row = 0;
                 int_type last_row = 0;
-                int_type thread_rows;
-                int cpu_count = std::thread::hardware_concurrency();
-                int max_threads = std::min(threads, cpu_count);
                 for (int i = 0; i < partitions_am; ++i) {
-                    first_row = last_row;
+                    first_rows[i] = last_row;
 
                     if (i == partitions - 1) last_row = m*n;
-                    else last_row = first_row + am_rows;
-                    thread_rows = last_row - first_row;
+                    else last_row = first_rows[i] + am_rows;
+                    partition_rows[i] = last_row - first_rows[i];
 
                     // Generate datastructures for this thread CRS (data_arr & col_ind are sometimes too large...)
-                    data_arr[i] = new T[5*thread_rows];
-                    row_start[i] = new int_type[thread_rows+1];
-                    col_ind[i] = new int_type[5*thread_rows];
+                    data_arr[i] = new T[5*partition_rows[i]];
+                    row_start[i] = new int_type[partition_rows[i]+1];
+                    col_ind[i] = new int_type[5*partition_rows[i]];
                     
                     // Fill CRS matrix for given thread
-                    pwm::fillPoisson(data_arr[i], row_start[i], col_ind [i], m, n, first_row, last_row);
-
-                    // Create mv lambda function for this thread
-                    std::function<void(const T*, T*)> mv_func = [=](const T* x, T* y) -> void {
-                        // Put the current thread on the right cpu
-                        cpu_set_t *mask;
-                        mask = CPU_ALLOC(1);
-                        auto mask_size = CPU_ALLOC_SIZE(1);
-                        CPU_ZERO_S(mask_size, mask);
-                        CPU_SET_S(i % max_threads, mask_size, mask);
-                        if (sched_setaffinity(0, mask_size, mask)) {
-                            std::cout << "Error in setAffinity" << std::endl;
-                        }
-
-                        int_type j;
-                        for (int_type l = 0; l < thread_rows; ++l) {
-                            T sum = 0;
-                            for (int_type k = row_start[i][l]; k < row_start[i][l+1]; ++k) {
-                                j = col_ind[i][k];
-                                sum += data_arr[i][k]*x[j];
-                            }
-                            y[l+first_row] = sum;
-                        }
-                    };
-
-                    mv_function_list.push_back(mv_func);
-
-
-                    // Create normalize function for this thread
-                    std::function<void(T*, T)> norm_func = [=](T* x, T norm) -> void {
-                        // Put the current thread on the right cpu
-                        cpu_set_t *mask;
-                        mask = CPU_ALLOC(1);
-                        auto mask_size = CPU_ALLOC_SIZE(1);
-                        CPU_ZERO_S(mask_size, mask);
-                        CPU_SET_S(i % max_threads, mask_size, mask);
-                        if (sched_setaffinity(0, mask_size, mask)) {
-                            std::cout << "Error in setAffinity" << std::endl;
-                        }
-
-                        for (int_type l = 0; l < thread_rows; ++l) {
-                            x[l+first_row] /= norm;
-                        }
-                    };
-
-                    norm_function_list.push_back(norm_func);
+                    pwm::fillPoisson(data_arr[i], row_start[i], col_ind [i], m, n, first_rows[i], last_row);
                 }
                 
+                // Generate functions for each partition
+                generateFunctions();
+            }
+
+            /**
+             * @brief Input the CRS matrix from a Triplet format
+             * 
+             * @param input Triplet format matrix used to convert to CRS
+             */
+            void loadFromTriplets(pwm::Triplet<T, int_type> input, const int partitions_am) {
+                this->noc = input.col_size;
+                this->nor = input.row_size;
+                this->nnz = input.nnz;
+
+                partitions = partitions_am;
+
+                row_start = new int_type*[partitions];
+                col_ind = new int_type*[partitions];
+                data_arr = new T*[partitions];
+                
+                partition_rows = new int_type[partitions];
+                first_rows = new int_type[partitions];
+
+                // Generate data for each thread
+                pwm::TripletToMultipleCRS(input.row_coord, input.col_coord, input.data, row_start, col_ind, data_arr, 
+                                          partitions, partition_rows, first_rows, this->nnz, this->nor);
+
+                // Generate function nodes per thread
+                generateFunctions();
             }
 
             /**
