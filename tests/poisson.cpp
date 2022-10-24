eb87c86b43af3e913eb0f762632f5553cb5e7a8c
KobeBergmans <r0747846@kuleuven.be>
2022-10-24 11:39:48 +0200

    Added more correct powermethod expectation values

--- tests/poisson.cpp
+++ tests/poisson.cpp
@@ -33,7 +33,7 @@ BOOST_AUTO_TEST_CASE(mv_size_3_3, * boost::unit_test::tolerance(std::pow(10, -14
 
     // Run test on all the matrices
     for (pwm::SparseMatrix<double, int>* mat: matrices) {
-        for (int partitions = 1; partitions <= std::max(omp_get_max_threads()*3, 3*3); ++partitions) {
+        for (int partitions = 1; partitions <= std::min(omp_get_max_threads()*3, 3*3); ++partitions) {
             mat->generatePoissonMatrix(3, 3, partitions);
             std::fill(x, x+mat_size, 1.);
             mat->mv(x,y);
@@ -61,7 +61,7 @@ BOOST_AUTO_TEST_CASE(mv_size_9_3, * boost::unit_test::tolerance(std::pow(10, -14
 
     // Run test on all the matrices
     for (pwm::SparseMatrix<double, int>* mat: matrices) {
-        for (int partitions = 1; partitions <= std::max(omp_get_max_threads()*3, 9*3); ++partitions) {
+        for (int partitions = 1; partitions <= std::min(omp_get_max_threads()*3, 9*3); ++partitions) {
             mat->generatePoissonMatrix(9, 3, partitions);
             std::fill(x, x+mat_size, 1.);
             mat->mv(x,y);
@@ -78,15 +78,12 @@ BOOST_AUTO_TEST_SUITE_END()
 
 BOOST_AUTO_TEST_SUITE(poisson_powermethod)
 
-BOOST_AUTO_TEST_CASE(powermethod_size_5_5, * boost::unit_test::tolerance(std::pow(10, -14))) {
+BOOST_AUTO_TEST_CASE(powermethod_size_5_5, * boost::unit_test::tolerance(std::pow(10, -10))) {
     int mat_size = 5*5;
 
     // Precomputed solution using matlab
-    double real_sol[] = {-0.083333333333333, 0.144337567297407,-0.166666666666667, 0.144337567297406,-0.083333333333333, 
-                         0.144337567297406, -0.250000000000000, 0.288675134594813, -0.250000000000000, 0.144337567297406, 
-                         -0.166666666666667, 0.288675134594813, -0.333333333333333, 0.288675134594813, -0.166666666666667, 
-                         0.144337567297406, -0.250000000000000, 0.288675134594813,-0.250000000000000, 0.144337567297406,
-                         -0.083333333333333, 0.144337567297406,-0.166666666666667, 0.144337567297406,-0.083333333333333};
+    double real_sol[] = {0.083333333337580, -0.144337567301084,  0.166666666668790, -0.144337567301084,  0.083333333337580, -0.144337567301084,  0.250000000000000, -0.288675134591135,  0.250000000000000, -0.144337567301084,  0.166666666668790, -0.288675134591135,  0.333333333324839, -0.288675134591135,  0.166666666668790, -0.144337567301084,  0.250000000000000, -0.288675134591135,  0.250000000000000, -0.144337567301084,  0.083333333337580, -0.144337567301084,  0.166666666668790, -0.144337567301084,
+   0.083333333337580};
 
 
     // Get datastructures
@@ -96,7 +93,7 @@ BOOST_AUTO_TEST_CASE(powermethod_size_5_5, * boost::unit_test::tolerance(std::po
 
     // Run test on all the matrices
     for (pwm::SparseMatrix<double, int>* mat: matrices) {
-        for (int partitions = 1; partitions <= std::max(omp_get_max_threads()*3, 5*5); ++partitions) {
+        for (int partitions = 1; partitions <= std::min(omp_get_max_threads()*3, 5*5); ++partitions) {
             mat->generatePoissonMatrix(5, 5, partitions);
             std::fill(x, x+mat_size, 1.);
             mat->powerMethod(x, y, 100);
@@ -109,23 +106,12 @@ BOOST_AUTO_TEST_CASE(powermethod_size_5_5, * boost::unit_test::tolerance(std::po
     }
 }
 
-BOOST_AUTO_TEST_CASE(powermethod_size_10_5, * boost::unit_test::tolerance(std::pow(10, -14))) {
+BOOST_AUTO_TEST_CASE(powermethod_size_10_5, * boost::unit_test::tolerance(std::pow(10, -10))) {
     int mat_size = 10*5;
 
     // Precomputed solution using matlab
-    double real_sol[] = {-0.034678880479031, 0.066548284304895,-0.093026341914262, 0.111967958550694,
-                         -0.121838597084438, 0.121838597084437,-0.111967958550693, 0.093026341914261,
-                         -0.066548284304894, 0.034678880479031, 0.060065582939291,-0.115265009572617, 
-                         0.161126350637777,-0.193934193029568, 0.211030640473159,-0.211030640473159, 
-                         0.193934193029566,-0.161126350637774, 0.115265009572615,-0.060065582939289,
-                         -0.069357760958063, 0.133096568609790,-0.186052683828525, 0.223935917101388,
-                         -0.243677194168875, 0.243677194168874,-0.223935917101386, 0.186052683828522,
-                         -0.133096568609787, 0.069357760958061, 0.060065582939291,-0.115265009572617, 
-                         0.161126350637776,-0.193934193029568, 0.211030640473159,-0.211030640473158, 
-                         0.193934193029565,-0.161126350637773, 0.115265009572614,-0.060065582939289,
-                         -0.034678880479032, 0.066548284304895,-0.093026341914262, 0.111967958550694,
-                         -0.121838597084438, 0.121838597084437,-0.111967958550692, 0.093026341914261,
-                         -0.066548284304893, 0.034678880479030};
+    double real_sol[] = {0.066549515155803, -0.111968981176319,  0.121838215857833, -0.093025002550037,  0.034678148918472,  0.034678148918472, -0.093025002550038,  0.121838215857834, -0.111968981176319,  0.066549515155803, -0.115267141466527,  0.193935964265070, -0.211029980164918,  0.161124030787534, -0.060064315837982, -0.060064315837983,  0.161124030787535, -0.211029980164918,  0.193935964265070, -0.115267141466527,  0.133099030307450, -0.223937962345645,  0.243676431708057, -0.186050005094264,  0.069356297834777,  0.069356297834778, -0.186050005094265,  0.243676431708058, -0.223937962345645,  0.133099030307450, -0.115267141466527,  0.193935964265070, -0.211029980164918,  0.161124030787534, -0.060064315837982, -0.060064315837983,  0.161124030787535, -0.211029980164918,  0.193935964265071, -0.115267141466527,  0.066549515155803, -0.111968981176319,  0.121838215857833, -0.093025002550037,  0.034678148918472,  0.034678148918472, -0.093025002550038,  0.121838215857834, -0.111968981176319, 
+   0.066549515155803};
 
 
     // Get datastructures
@@ -135,7 +121,7 @@ BOOST_AUTO_TEST_CASE(powermethod_size_10_5, * boost::unit_test::tolerance(std::p
 
     // Run test on all the matrices
     for (pwm::SparseMatrix<double, int>* mat: matrices) {
-        for (int partitions = 1; partitions <= std::max(omp_get_max_threads()*3, 10*5); ++partitions) {
+        for (int partitions = 1; partitions <= std::min(omp_get_max_threads()*3, 10*5); ++partitions) {
             mat->generatePoissonMatrix(10, 5, partitions);
             std::fill(x, x+mat_size, 1.);
             mat->powerMethod(x, y, 100);
