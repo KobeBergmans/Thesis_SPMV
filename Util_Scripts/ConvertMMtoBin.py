e8ba19e627ee7f740dd5d509e514a94f27c83c69
KobeBergmans <r0747846@kuleuven.be>
2022-11-11 16:16:46 +0100

    Updated conversion script for symmetric matrices

--- Util_Scripts/ConvertMMtoBin.py
+++ Util_Scripts/ConvertMMtoBin.py
@@ -3,8 +3,16 @@ import scipy.sparse
 import numpy as np
 import sys
 
+# Arguments:
+# 1. Input file
+# 2. Output file
+# 3. If this is 1 we have a symmetric input matrix
+
 if __name__ == "__main__":
     A = scipy.io.mmread(sys.argv[1])
+    
+    if int(sys.argv[3]) == 1:
+        A = scipy.sparse.tril(A)
     
     row = A.row.astype(np.uint32)
     col = A.col.astype(np.uint32)
