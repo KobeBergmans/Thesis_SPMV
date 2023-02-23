8b6281aba9f6b901ec25d60ce1766c719e5f6719
KobeBergmans <r0747846@kuleuven.be>
2023-02-23 09:18:43 +0100

    Final try

--- Tests/tbb_test.cpp
+++ Tests/tbb_test.cpp
@@ -4,7 +4,7 @@
 // #include "oneapi/tbb.h"
 #include <tbb/parallel_for.h>
 
-int main(int argc, char** argv) {
+int main() {
     tbb::parallel_for(0, 10, [=](int i) {
         sleep(1);
         std::cout << "Iteration: " << i << std::endl;
