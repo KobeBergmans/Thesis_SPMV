def44b1ff760a46a2658ef47dd238a6e272dc879
KobeBergmans <r0747846@kuleuven.be>
2023-02-23 09:14:41 +0100

    Further testing on docker

--- Tests/tbb_test.cpp
+++ Tests/tbb_test.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include <unistd.h>
 
 // #include "oneapi/tbb.h"
 #include <tbb/parallel_for.h>
 
 int main(int argc, char** argv) {
     tbb::parallel_for(0, 10, [=](int i) {
+        sleep(1);
         std::cout << "Iteration: " << i << std::endl;
     });
 
