0b9a02385fdd205bd24909eaa32676d275ed3782
KobeBergmans <r0747846@kuleuven.be>
2023-03-06 20:17:00 +0100

    Made CRSOMP memory safe

--- Env_Implementations/CRSOMP.hpp
+++ Env_Implementations/CRSOMP.hpp
@@ -43,6 +43,17 @@ namespace pwm {
             // Base constructor
             CRSOMP(int threads): threads(threads) {}
 
+            // Deconstructor
+            ~CRSOMP() {
+                delete [] row_start;
+                delete [] col_ind;
+                delete [] data_arr;
+
+                row_start = NULL;
+                col_ind = NULL;
+                data_arr = NULL;
+            }
+
             /**
              * @brief Fill the given matrix as a 2D discretized poisson matrix with equal discretization steplength in x and y
              *
