4f33dc6c06aaedc96c9e515b7a1c197c69805d7d
KobeBergmans <r0747846@kuleuven.be>
2023-03-20 10:33:55 +0100

    Formatted timing results

--- Timing_Scripts/run_timings_spmv.sh
+++ Timing_Scripts/run_timings_spmv.sh
@@ -65,13 +65,17 @@ echo "Running Block CO-H implementation with 2-8 threads"
 ./driver_spmv_input $1 $2 $3 10 7
 ./driver_spmv_input $1 $2 $3 10 8
 
-echo "Running CRS_merge, CSB, Block CO-H with 10, 12 and 14 threads"
+echo "Running CRS_merge with 10, 12 and 14 threads"
 ./driver_spmv_input $1 $2 $3 8 10
 ./driver_spmv_input $1 $2 $3 8 12
 ./driver_spmv_input $1 $2 $3 8 14
+
+echo "Running CSB with 10, 12, 14 threads"
 ./driver_spmv_input $1 $2 $3 9 10
 ./driver_spmv_input $1 $2 $3 9 12
 ./driver_spmv_input $1 $2 $3 9 14
+
+echo "Running Block CO-H with 10, 12 and 14 threads"
 ./driver_spmv_input $1 $2 $3 10 10
 ./driver_spmv_input $1 $2 $3 10 12
 ./driver_spmv_input $1 $2 $3 10 14
