c1d1b81dceacf697eb4072283fd26b9e6e4a37f6
KobeBergmans <r0747846@kuleuven.be>
2023-03-21 15:20:58 +0100

    Updated run_sniper script to make needed directories and include an option for the config file

--- Timing_Scripts/run_sniper.sh
+++ Timing_Scripts/run_sniper.sh
@@ -1,7 +1,14 @@
 rm -f driver_sniper
 make driver_sniper
 
-../snipersim/run-sniper -c modern-cpu.cfg -n $2 -d ./Sniper_Out/CRSOMP/ -s markers -s roi-iter::2:2 --roi-script --no-cache-warming -- ./driver_sniper $1 2 $2
-../snipersim/run-sniper -c modern-cpu.cfg -n $2 -d ./Sniper_Out/BlockCOH/ -s markers -s roi-iter::2:2 --roi-script --no-cache-warming -- ./driver_sniper $1 8 $2
-../snipersim/run-sniper -c modern-cpu.cfg -n $2 -d ./Sniper_Out/CSB/ -s markers -s roi-iter::2:2 --roi-script --no-cache-warming -- ./driver_sniper $1 9 $2
-../snipersim/run-sniper -c modern-cpu.cfg -n $2 -d ./Sniper_Out/MergeCRS -s markers -s roi-iter::2:2 --roi-script --no-cache-warming -- ./driver_sniper $1 10 $2
+rm -rf Sniper_Out
+mkdir Sniper_Out
+mkdir Sniper_Out/CRSOMP
+mkdir Sniper_Out/BlockCOH
+mkdir Sniper_Out/CSB
+mkdir Sniper_Out/MergeCRS
+
+../snipersim/run-sniper -c $3 -n $2 -d ./Sniper_Out/CRSOMP/ -s markers -s roi-iter::2:2 --roi-script --no-cache-warming -- ./driver_sniper $1 2 $2
+../snipersim/run-sniper -c $3 -n $2 -d ./Sniper_Out/BlockCOH/ -s markers -s roi-iter::2:2 --roi-script --no-cache-warming -- ./driver_sniper $1 8 $2
+../snipersim/run-sniper -c $3 -n $2 -d ./Sniper_Out/CSB/ -s markers -s roi-iter::2:2 --roi-script --no-cache-warming -- ./driver_sniper $1 9 $2
+../snipersim/run-sniper -c $3 -n $2 -d ./Sniper_Out/MergeCRS -s markers -s roi-iter::2:2 --roi-script --no-cache-warming -- ./driver_sniper $1 10 $2
