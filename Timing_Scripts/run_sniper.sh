337820936b879469957a1a03a4a0af51a3e9de1f
KobeBergmans <r0747846@kuleuven.be>
2023-03-28 12:59:38 +0200

    Filling input vector is also done in cache warming mode because it takes too long...

--- Timing_Scripts/run_sniper.sh
+++ Timing_Scripts/run_sniper.sh
@@ -8,25 +8,25 @@ mkdir Sniper_Out/BlockCOH
 mkdir Sniper_Out/CSB
 mkdir Sniper_Out/MergeCRS
 
-../snipersim/run-sniper -c $3 -n $2 -d ./Sniper_Out/CRSOMP/ -s markers:verbose:stats -s roi-iter::2:3 --roi-script --no-cache-warming -- ./driver_sniper $1 2 $2
+../snipersim/run-sniper -c $3 -n $2 -d ./Sniper_Out/CRSOMP/ -s markers:verbose:stats -s roi-iter::3:3 --roi-script --no-cache-warming -- ./driver_sniper $1 2 $2
 cd Sniper_Out/CRSOMP
 ${SNIPER}/tools/cpistack.py --partial="marker-1-3:marker-2-3"
 ${SNIPER}/tools/gen_simout.py --partial="marker-1-3:marker-2-3" > part_sim.out
 cd ../..
 
-../snipersim/run-sniper -c $3 -n $2 -d ./Sniper_Out/BlockCOH/ -s markers:verbose:stats -s roi-iter::2:3 --roi-script --no-cache-warming -- ./driver_sniper $1 8 $2
+../snipersim/run-sniper -c $3 -n $2 -d ./Sniper_Out/BlockCOH/ -s markers:verbose:stats -s roi-iter::3:3 --roi-script --no-cache-warming -- ./driver_sniper $1 8 $2
 cd Sniper_Out/BlockCOH
 ${SNIPER}/tools/cpistack.py --partial="marker-1-3:marker-2-3"
 ${SNIPER}/tools/gen_simout.py --partial="marker-1-3:marker-2-3" > part_sim.out
 cd ../..
 
-../snipersim/run-sniper -c $3 -n $2 -d ./Sniper_Out/CSB/ -s markers:verbose:stats -s roi-iter::2:3 --roi-script --no-cache-warming -- ./driver_sniper $1 9 $2
+../snipersim/run-sniper -c $3 -n $2 -d ./Sniper_Out/CSB/ -s markers:verbose:stats -s roi-iter::3:3 --roi-script --no-cache-warming -- ./driver_sniper $1 9 $2
 cd Sniper_Out/CSB
 ${SNIPER}/tools/cpistack.py --partial="marker-1-3:marker-2-3"
 ${SNIPER}/tools/gen_simout.py --partial="marker-1-3:marker-2-3" > part_sim.out
 cd ../..
 
-../snipersim/run-sniper -c $3 -n $2 -d ./Sniper_Out/MergeCRS -s markers:verbose:stats -s roi-iter::2:3 --roi-script --no-cache-warming -- ./driver_sniper $1 10 $2
+../snipersim/run-sniper -c $3 -n $2 -d ./Sniper_Out/MergeCRS -s markers:verbose:stats -s roi-iter::3:3 --roi-script --no-cache-warming -- ./driver_sniper $1 10 $2
 cd Sniper_Out/MergeCRS
 ${SNIPER}/tools/cpistack.py --partial="marker-1-3:marker-2-3"
 ${SNIPER}/tools/gen_simout.py --partial="marker-1-3:marker-2-3" > part_sim.out
