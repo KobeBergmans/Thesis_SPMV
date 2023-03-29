e26886e282b6a77bc9fb9ba263a86d6c59ec56a4
KobeBergmans <r0747846@kuleuven.be>
2023-03-29 23:23:06 +0200

    Added optional folder seleciton to run_sniper script

--- Timing_Scripts/run_sniper.sh
+++ Timing_Scripts/run_sniper.sh
@@ -1,32 +1,32 @@
 rm -f driver_sniper
 make driver_sniper
 
-rm -rf Sniper_Out
-mkdir Sniper_Out
-mkdir Sniper_Out/CRSOMP
-mkdir Sniper_Out/BlockCOH
-mkdir Sniper_Out/CSB
-mkdir Sniper_Out/MergeCRS
+rm -rf Sniper_Out/$4
+mkdir Sniper_Out/$4
+mkdir Sniper_Out/$4/CRSOMP
+mkdir Sniper_Out/$4/BlockCOH
+mkdir Sniper_Out/$4/CSB
+mkdir Sniper_Out/$4/MergeCRS
 
-../snipersim/run-sniper -c $3 -n $2 -d ./Sniper_Out/CRSOMP/ -s markers:verbose:stats -s roi-iter::3:3 --roi-script --no-cache-warming -- ./driver_sniper $1 2 $2
+../snipersim/run-sniper -c $3 -n $2 -d ./Sniper_Out/$4/CRSOMP/ -s markers:verbose:stats -s roi-iter::3:3 --roi-script --no-cache-warming -- ./driver_sniper $1 2 $2
 cd Sniper_Out/CRSOMP
 ${SNIPER}/tools/cpistack.py --partial="marker-1-3:marker-2-3"
 ${SNIPER}/tools/gen_simout.py --partial="marker-1-3:marker-2-3" > part_sim.out
 cd ../..
 
-../snipersim/run-sniper -c $3 -n $2 -d ./Sniper_Out/BlockCOH/ -s markers:verbose:stats -s roi-iter::3:3 --roi-script --no-cache-warming -- ./driver_sniper $1 8 $2
+../snipersim/run-sniper -c $3 -n $2 -d ./Sniper_Out/$4/BlockCOH/ -s markers:verbose:stats -s roi-iter::3:3 --roi-script --no-cache-warming -- ./driver_sniper $1 8 $2
 cd Sniper_Out/BlockCOH
 ${SNIPER}/tools/cpistack.py --partial="marker-1-3:marker-2-3"
 ${SNIPER}/tools/gen_simout.py --partial="marker-1-3:marker-2-3" > part_sim.out
 cd ../..
 
-../snipersim/run-sniper -c $3 -n $2 -d ./Sniper_Out/CSB/ -s markers:verbose:stats -s roi-iter::3:3 --roi-script --no-cache-warming -- ./driver_sniper $1 9 $2
+../snipersim/run-sniper -c $3 -n $2 -d ./Sniper_Out/$4/CSB/ -s markers:verbose:stats -s roi-iter::3:3 --roi-script --no-cache-warming -- ./driver_sniper $1 9 $2
 cd Sniper_Out/CSB
 ${SNIPER}/tools/cpistack.py --partial="marker-1-3:marker-2-3"
 ${SNIPER}/tools/gen_simout.py --partial="marker-1-3:marker-2-3" > part_sim.out
 cd ../..
 
-../snipersim/run-sniper -c $3 -n $2 -d ./Sniper_Out/MergeCRS -s markers:verbose:stats -s roi-iter::3:3 --roi-script --no-cache-warming -- ./driver_sniper $1 10 $2
+../snipersim/run-sniper -c $3 -n $2 -d ./Sniper_Out/$4/MergeCRS -s markers:verbose:stats -s roi-iter::3:3 --roi-script --no-cache-warming -- ./driver_sniper $1 10 $2
 cd Sniper_Out/MergeCRS
 ${SNIPER}/tools/cpistack.py --partial="marker-1-3:marker-2-3"
 ${SNIPER}/tools/gen_simout.py --partial="marker-1-3:marker-2-3" > part_sim.out
