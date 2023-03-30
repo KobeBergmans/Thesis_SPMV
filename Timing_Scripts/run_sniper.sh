0e3e35247c1cdb1a68382604c7fd83fc62510a98
KobeBergmans <r0747846@kuleuven.be>
2023-03-30 11:17:01 +0200

    Fixed run_sniper script

--- Timing_Scripts/run_sniper.sh
+++ Timing_Scripts/run_sniper.sh
@@ -9,25 +9,25 @@ mkdir Sniper_Out/$4/CSB
 mkdir Sniper_Out/$4/MergeCRS
 
 ../snipersim/run-sniper -c $3 -n $2 -d ./Sniper_Out/$4/CRSOMP/ -s markers:verbose:stats -s roi-iter::3:3 --roi-script --no-cache-warming -- ./driver_sniper $1 2 $2
-cd Sniper_Out/CRSOMP
+cd Sniper_Out/$4/CRSOMP
 ${SNIPER}/tools/cpistack.py --partial="marker-1-3:marker-2-3"
 ${SNIPER}/tools/gen_simout.py --partial="marker-1-3:marker-2-3" > part_sim.out
-cd ../..
+cd ../../..
 
 ../snipersim/run-sniper -c $3 -n $2 -d ./Sniper_Out/$4/BlockCOH/ -s markers:verbose:stats -s roi-iter::3:3 --roi-script --no-cache-warming -- ./driver_sniper $1 8 $2
-cd Sniper_Out/BlockCOH
+cd Sniper_Out/$4/BlockCOH
 ${SNIPER}/tools/cpistack.py --partial="marker-1-3:marker-2-3"
 ${SNIPER}/tools/gen_simout.py --partial="marker-1-3:marker-2-3" > part_sim.out
-cd ../..
+cd ../../..
 
 ../snipersim/run-sniper -c $3 -n $2 -d ./Sniper_Out/$4/CSB/ -s markers:verbose:stats -s roi-iter::3:3 --roi-script --no-cache-warming -- ./driver_sniper $1 9 $2
-cd Sniper_Out/CSB
+cd Sniper_Out/$4/CSB
 ${SNIPER}/tools/cpistack.py --partial="marker-1-3:marker-2-3"
 ${SNIPER}/tools/gen_simout.py --partial="marker-1-3:marker-2-3" > part_sim.out
-cd ../..
+cd ../../..
 
 ../snipersim/run-sniper -c $3 -n $2 -d ./Sniper_Out/$4/MergeCRS -s markers:verbose:stats -s roi-iter::3:3 --roi-script --no-cache-warming -- ./driver_sniper $1 10 $2
-cd Sniper_Out/MergeCRS
+cd Sniper_Out/$4/MergeCRS
 ${SNIPER}/tools/cpistack.py --partial="marker-1-3:marker-2-3"
 ${SNIPER}/tools/gen_simout.py --partial="marker-1-3:marker-2-3" > part_sim.out
-cd ../..
+cd ../../..
