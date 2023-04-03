b4d9fbcc5a7e2b29642b6d62b50e0a3ddbdbca6f
KobeBergmans <r0747846@kuleuven.be>
2023-04-03 14:20:54 +0200

    Fixed bug in run_sniper script that made that mergeCRS and blockCOH were swapped

--- Timing_Scripts/run_sniper.sh
+++ Timing_Scripts/run_sniper.sh
@@ -4,9 +4,9 @@ make driver_sniper
 rm -rf Sniper_Out/$4
 mkdir Sniper_Out/$4
 mkdir Sniper_Out/$4/CRSOMP
-mkdir Sniper_Out/$4/BlockCOH
-mkdir Sniper_Out/$4/CSB
 mkdir Sniper_Out/$4/MergeCRS
+mkdir Sniper_Out/$4/CSB
+mkdir Sniper_Out/$4/BlockCOH
 
 ../snipersim/run-sniper -c $3 -n $2 -d ./Sniper_Out/$4/CRSOMP/ -s markers:verbose:stats -s roi-iter::3:3 --roi-script --no-cache-warming -- ./driver_sniper $1 2 $2
 cd Sniper_Out/$4/CRSOMP
@@ -14,8 +14,8 @@ ${SNIPER}/tools/cpistack.py --partial="marker-1-3:marker-2-3"
 ${SNIPER}/tools/gen_simout.py --partial="marker-1-3:marker-2-3" > part_sim.out
 cd ../../..
 
-../snipersim/run-sniper -c $3 -n $2 -d ./Sniper_Out/$4/BlockCOH/ -s markers:verbose:stats -s roi-iter::3:3 --roi-script --no-cache-warming -- ./driver_sniper $1 8 $2
-cd Sniper_Out/$4/BlockCOH
+../snipersim/run-sniper -c $3 -n $2 -d ./Sniper_Out/$4/MergeCRS/ -s markers:verbose:stats -s roi-iter::3:3 --roi-script --no-cache-warming -- ./driver_sniper $1 8 $2
+cd Sniper_Out/$4/MergeCRS
 ${SNIPER}/tools/cpistack.py --partial="marker-1-3:marker-2-3"
 ${SNIPER}/tools/gen_simout.py --partial="marker-1-3:marker-2-3" > part_sim.out
 cd ../../..
@@ -26,8 +26,8 @@ ${SNIPER}/tools/cpistack.py --partial="marker-1-3:marker-2-3"
 ${SNIPER}/tools/gen_simout.py --partial="marker-1-3:marker-2-3" > part_sim.out
 cd ../../..
 
-../snipersim/run-sniper -c $3 -n $2 -d ./Sniper_Out/$4/MergeCRS -s markers:verbose:stats -s roi-iter::3:3 --roi-script --no-cache-warming -- ./driver_sniper $1 10 $2
-cd Sniper_Out/$4/MergeCRS
+../snipersim/run-sniper -c $3 -n $2 -d ./Sniper_Out/$4/BlockCOH -s markers:verbose:stats -s roi-iter::3:3 --roi-script --no-cache-warming -- ./driver_sniper $1 10 $2
+cd Sniper_Out/$4/BlockCOH
 ${SNIPER}/tools/cpistack.py --partial="marker-1-3:marker-2-3"
 ${SNIPER}/tools/gen_simout.py --partial="marker-1-3:marker-2-3" > part_sim.out
 cd ../../..
