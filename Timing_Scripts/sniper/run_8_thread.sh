4183ff92f4f245629b6bdf0ffc6a153b6707a07b
KobeBergmans <r0747846@kuleuven.be>
2023-04-03 16:25:06 +0200

    Bugfix in run_8_thread sniepr script

--- Timing_Scripts/sniper/run_8_thread.sh
+++ Timing_Scripts/sniper/run_8_thread.sh
@@ -15,13 +15,13 @@ mkdir Sniper_Out/$3/MergeCRS
 mkdir Sniper_Out/$3/CSB
 mkdir Sniper_Out/$3/BlockCOH
 
-../snipersim/run-sniper -c $2 -n 2 -d ./Sniper_Out/$3/CRSOMP/ -s markers:verbose:stats -s roi-iter::3:3 --roi-script --no-cache-warming -- ./driver_sniper $1 2 8 &
-../snipersim/run-sniper -c $2 -n 2 -d ./Sniper_Out/$3/MergeCRS/ -s markers:verbose:stats -s roi-iter::3:3 --roi-script --no-cache-warming -- ./driver_sniper $1 8 8
+../snipersim/run-sniper -c $2 -n 8 -d ./Sniper_Out/$3/CRSOMP/ -s markers:verbose:stats -s roi-iter::3:3 --roi-script --no-cache-warming -- ./driver_sniper $1 2 8 &
+../snipersim/run-sniper -c $2 -n 8 -d ./Sniper_Out/$3/MergeCRS/ -s markers:verbose:stats -s roi-iter::3:3 --roi-script --no-cache-warming -- ./driver_sniper $1 8 8
 
 wait 
 
-../snipersim/run-sniper -c $2 -n 2 -d ./Sniper_Out/$3/CSB/ -s markers:verbose:stats -s roi-iter::3:3 --roi-script --no-cache-warming -- ./driver_sniper $1 9 8 &
-../snipersim/run-sniper -c $2 -n 2 -d ./Sniper_Out/$3/BlockCOH -s markers:verbose:stats -s roi-iter::3:3 --roi-script --no-cache-warming -- ./driver_sniper $1 10 8
+../snipersim/run-sniper -c $2 -n 8 -d ./Sniper_Out/$3/CSB/ -s markers:verbose:stats -s roi-iter::3:3 --roi-script --no-cache-warming -- ./driver_sniper $1 9 8 &
+../snipersim/run-sniper -c $2 -n 8 -d ./Sniper_Out/$3/BlockCOH -s markers:verbose:stats -s roi-iter::3:3 --roi-script --no-cache-warming -- ./driver_sniper $1 10 8
 
 wait
 
