173dfa0c0a85c6fd337a106deba6018dec0b4c9a
KobeBergmans <r0747846@kuleuven.be>
2023-04-04 12:36:02 +0200

    Updated the generate matrix script to be more robust for matrices with a size which is no multiple of the blocksize

--- Util_Scripts/generateRandMat.m
+++ Util_Scripts/generateRandMat.m
@@ -9,6 +9,11 @@ block_bits = min(16, 3+lg_sqrt_size);
 block_size = 2.^block_bits;
 nb_blocks = ceil(size / block_size);
 
+block_compensation = 1;
+if mod(size, block_size) ~= 0 
+    block_compensation = mod(size, block_size) / block_size;
+end
+
 row_cts = max(zeros(size, 1), round(sqrt(var_line)*randn(size, 1) + avg_line));
 nnz = sum(row_cts);
 
@@ -19,47 +24,73 @@ for block_row = 1:nb_blocks
     block_row_cts{block_row} = row_cts(row_start:row_end);
 end
 
-avg_block = nnz / nb_blocks.^2;
-block_cts = max(zeros(nb_blocks, nb_blocks), round(sqrt(var_block)*randn(nb_blocks, nb_blocks) + avg_block));
+avg_block = (nnz / (size.^2)) * block_size.^2;
+block_cts = max(zeros(nb_blocks, nb_blocks), sqrt(var_block)*randn(nb_blocks, nb_blocks) + avg_block);
+
+for block_index = 1:nb_blocks
+    block_cts(end, block_index) = block_cts(end, block_index) * block_compensation;
+    block_cts(block_index, end) = block_cts(block_index, end) * block_compensation;
+end
+
+block_cts = round(block_cts);
 
 A_parts = cell(nb_blocks, 1);
-parfor block_row = 1:nb_blocks
+for block_row = 1:nb_blocks
     disp(['block row ', num2str(block_row), ' starting...']);
     block_row_nnz = sum(block_row_cts{block_row});
-    
+
     if block_row ~= nb_blocks
         curr_row_cts = zeros(block_size, 1);
+        rel_row_err = ones(block_size, 1);
     else
         am_rows = mod(size, block_size);
         if am_rows == 0
             am_rows = block_size;
         end
         curr_row_cts = zeros(am_rows, 1);
+        rel_row_err = ones(am_rows, 1);
     end
-    curr_block_cts = zeros(1, nb_blocks);
 
-    A_temp = spalloc(block_size, size, block_row_nnz);
+    curr_block_cts = zeros(1, nb_blocks);
+    rel_block_err = ones(1, nb_blocks);
+
+    if (block_row ~= nb_blocks) || (mod(size, block_size) == 0)
+        A_temp = spalloc(block_size, size, block_row_nnz);
+    else
+        A_temp = spalloc(mod(size, block_size), size, block_row_nnz);
+    end
     
     for i=1:block_row_nnz
         if mod(i, 10000) == 0
             disp(['block row ', num2str(block_row), ' has processed ', num2str(i), ' nnz']);
         end
 
-        % Find row which needs the most nnz
-        [~, row_ind] = max(block_row_cts{block_row} - curr_row_cts);
-        
-        % Find block col which needs the most nnz
-        [~, block_col_index] = max(block_cts(block_row, :) - curr_block_cts);
+        % Find row and block col which needs the most nnz by checking the 
+        % relative error
+        [~, row_ind] = max(rel_row_err);
+        [~, block_col_index] = max(rel_block_err);
     
         % Generate a random column index in this block col
-        col_ind = min(size, block_size*(block_col_index-1) + randi(block_size));
+        if (block_col_index ~= nb_blocks) || (mod(size, block_size) == 0)
+            col_ind = block_size*(block_col_index-1) + randi(block_size);
+        else 
+            col_ind = block_size*(block_col_index-1) + randi(mod(size, block_size));
+        end
+
         while A_temp(row_ind, col_ind) ~= 0
-            col_ind = min(size, block_size*(block_col_index-1) + randi(block_size));
+            if (block_col_index ~= nb_blocks) || (mod(size, block_size) == 0)
+                col_ind = block_size*(block_col_index-1) + randi(block_size);
+            else 
+                col_ind = block_size*(block_col_index-1) + randi(mod(size, block_size));
+            end
         end
         A_temp(row_ind, col_ind) = 1;
     
         curr_row_cts(row_ind) = curr_row_cts(row_ind) + 1;
+        rel_row_err(row_ind) = (block_row_cts{block_row}(row_ind) - curr_row_cts(row_ind)) ./ block_row_cts{block_row}(row_ind);
+
         curr_block_cts(block_col_index) = curr_block_cts(block_col_index) + 1;
+        rel_block_err(block_col_index) = (block_cts(block_row, block_col_index) - curr_block_cts(block_col_index)) ./ block_cts(block_row, block_col_index);
     end
 
     A_parts{block_row} = A_temp;
@@ -71,4 +102,3 @@ for block_row = 2:nb_blocks
 end
 
 end
-
