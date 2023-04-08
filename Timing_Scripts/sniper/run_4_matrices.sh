84968e97bc0f9c41a2c66ab0a701c91076a8957a
KobeBergmans <r0747846@kuleuven.be>
2023-04-08 19:15:24 +0200

    Added option to append extra suffix to file names

--- Timing_Scripts/sniper/run_4_matrices.sh
+++ Timing_Scripts/sniper/run_4_matrices.sh
@@ -1,19 +1,19 @@
 # Runs simulations for the 4 given matrices with a given config
-# (Takes approximately 10h for test matrices and uses 32 threads)
 # Inputs:
 #   - Matrix input file 1
 #   - Matrix input file 2
 #   - Matrix input file 3
 #   - Matrix input file 4
 #   - Sniper config file
+#   - Extra suffix to folder names to have more simulations than 1 run
 
-sh Timing_Scripts/sniper/run_2_thread.sh $1 $5 "2_thread_1" & 
-sh Timing_Scripts/sniper/run_2_thread.sh $2 $5 "2_thread_2" &
-sh Timing_Scripts/sniper/run_2_thread.sh $3 $5 "2_thread_3" & 
-sh Timing_Scripts/sniper/run_2_thread.sh $4 $5 "2_thread_4"
+sh Timing_Scripts/sniper/run_2_thread.sh $1 $5 "2_thread_1_$6" & 
+sh Timing_Scripts/sniper/run_2_thread.sh $2 $5 "2_thread_2_$6" &
+sh Timing_Scripts/sniper/run_2_thread.sh $3 $5 "2_thread_3_$6" & 
+sh Timing_Scripts/sniper/run_2_thread.sh $4 $5 "2_thread_4_$6"
 wait
 
-sh Timing_Scripts/sniper/run_8_thread.sh $1 $5 "8_thread_1"
-sh Timing_Scripts/sniper/run_8_thread.sh $2 $5 "8_thread_2"
-sh Timing_Scripts/sniper/run_8_thread.sh $3 $5 "8_thread_3"
-sh Timing_Scripts/sniper/run_8_thread.sh $4 $5 "8_thread_4"
+sh Timing_Scripts/sniper/run_8_thread.sh $1 $5 "8_thread_1_$6"
+sh Timing_Scripts/sniper/run_8_thread.sh $2 $5 "8_thread_2_$6"
+sh Timing_Scripts/sniper/run_8_thread.sh $3 $5 "8_thread_3_$6"
+sh Timing_Scripts/sniper/run_8_thread.sh $4 $5 "8_thread_4_$6"
