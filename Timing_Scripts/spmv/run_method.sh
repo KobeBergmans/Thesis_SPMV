418fd68322e99f14a2005749532289c75e7baebe
KobeBergmans <r0747846@kuleuven.be>
2023-04-11 18:07:34 +0200

    Changed spmv run method script such that it can take an arbirary amount of threads

--- Timing_Scripts/spmv/run_method.sh
+++ Timing_Scripts/spmv/run_method.sh
@@ -1,12 +1,17 @@
+# Runs spmv input driver for a given matrix and a specified amount of times for a given method using a given amount of threads
+# Inputs:
+# - Matrix file
+# - Amount of timed runs
+# - Amount of warmup runs
+# - Method
+# - Starting thread amount
+# - End thread amount
+
 rm -f driver_spmv_input
 make driver_spmv_input
 
-echo "Running method $4 with 2-8 threads"
-
-./driver_spmv_input $1 $2 $3 $4 2
-./driver_spmv_input $1 $2 $3 $4 3
-./driver_spmv_input $1 $2 $3 $4 4
-./driver_spmv_input $1 $2 $3 $4 5
-./driver_spmv_input $1 $2 $3 $4 6
-./driver_spmv_input $1 $2 $3 $4 7
-./driver_spmv_input $1 $2 $3 $4 8
+echo "Running method $4"
+for thread in $(seq $4 $5)
+do
+./driver_spmv_input $1 $2 $3 $4 $thread
+done
