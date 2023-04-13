b157cea7b4cc9f9301240a65fcf2c6224a334381
KobeBergmans <r0747846@kuleuven.be>
2023-04-13 18:45:07 +0200

    Added script that runs 5 matrices

---
+++ Timing_Scripts/sniper/run_5_matrices_8.sh
@@ -0,0 +1,22 @@
+# Runs simulations for the 7 given matrices with a given config
+# Inputs:
+#   - Matrix input file 1
+#   - Matrix input file 2
+#   - Matrix input file 3
+#   - Matrix input file 4
+#   - Matrix input file 5
+#   - Sniper config file
+#   - Extra suffix to folder names to have more simulations than 1 run
+
+rm -rf driver_sniper
+make driver_sniper
+
+sh Timing_Scripts/sniper/run_8_thread.sh $1 $6 "8_thread_1_$7" &
+sh Timing_Scripts/sniper/run_8_thread.sh $2 $6 "8_thread_2_$7"
+wait
+
+sh Timing_Scripts/sniper/run_8_thread.sh $3 $6 "8_thread_3_$7" &
+sh Timing_Scripts/sniper/run_8_thread.sh $4 $6 "8_thread_4_$7"
+wait
+
+sh Timing_Scripts/sniper/run_8_thread.sh $5 $6 "8_thread_5_$7"
