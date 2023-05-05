e34f556dbe48339573fb1fc2788a4f36acaf81a2
KobeBergmans <r0747846@kuleuven.be>
2023-05-05 17:02:42 +0200

    Added script to run 256 core machines

---
+++ Timing_Scripts/sniper/thread/run_256_thread.sh
@@ -0,0 +1,38 @@
+# Runs simulations for the given config for the 4 methods using 256 threads with the first and last methods in parallel
+# Inputs:
+#   - Matrix input file
+#   - Sniper Config file
+#   - Output directory name
+
+rm -rf Sniper_Out/$3
+mkdir Sniper_Out/$3
+mkdir Sniper_Out/$3/CRSOMP
+mkdir Sniper_Out/$3/MergeCRS
+mkdir Sniper_Out/$3/CSB
+mkdir Sniper_Out/$3/BlockCOH
+
+../snipersim/run-sniper -c $2 -n 256 -d ./Sniper_Out/$3/CRSOMP/ -s markers:verbose:stats -s roi-iter::2:2 --roi-script --no-cache-warming -- ./driver_sniper $1 2 256 &
+../snipersim/run-sniper -c $2 -n 256 -d ./Sniper_Out/$3/MergeCRS/ -s markers:verbose:stats -s roi-iter::2:2 --roi-script --no-cache-warming -- ./driver_sniper $1 8 256 &
+../snipersim/run-sniper -c $2 -n 256 -d ./Sniper_Out/$3/CSB/ -s markers:verbose:stats -s roi-iter::2:2 --roi-script --no-cache-warming -- ./driver_sniper $1 9 256 &
+../snipersim/run-sniper -c $2 -n 256 -d ./Sniper_Out/$3/BlockCOH -s markers:verbose:stats -s roi-iter::2:2 --roi-script --no-cache-warming -- ./driver_sniper $1 10 256
+wait
+
+cd Sniper_Out/$3/CRSOMP
+${SNIPER}/tools/cpistack.py --partial="marker-1-2:marker-2-2"
+${SNIPER}/tools/gen_simout.py --partial="marker-1-2:marker-2-2" > part_sim.out
+cd ../../..
+
+cd Sniper_Out/$3/MergeCRS
+${SNIPER}/tools/cpistack.py --partial="marker-1-2:marker-2-2"
+${SNIPER}/tools/gen_simout.py --partial="marker-1-2:marker-2-2" > part_sim.out
+cd ../../..
+
+cd Sniper_Out/$3/CSB
+${SNIPER}/tools/cpistack.py --partial="marker-1-2:marker-2-2"
+${SNIPER}/tools/gen_simout.py --partial="marker-1-2:marker-2-2" > part_sim.out
+cd ../../..
+
+cd Sniper_Out/$3/BlockCOH
+${SNIPER}/tools/cpistack.py --partial="marker-1-2:marker-2-2"
+${SNIPER}/tools/gen_simout.py --partial="marker-1-2:marker-2-2" > part_sim.out
+cd ../../..
